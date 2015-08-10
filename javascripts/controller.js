b0da27666b016644d40634a1752787f02a86bb99
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2015-08-10 14:49:43 +0900

    Update Demo
    Add the feature of selecting stroke color.

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -3,9 +3,29 @@ $(function() {
     var WIDTH  = 300;
     var HEIGHT = 300;
 
+    var PALLETE = [
+        ['rgb(255,   0, 0)', 'rgb(  0, 255,   0)', 'rgb(  0,   0, 255)'],
+        ['rgb(255, 255, 0)', 'rgb(  0, 255, 255)', 'rgb(255,   0, 255)'],
+        ['rgb(  0,   0, 0)', 'rgb(127, 127, 127)', 'rgb(255, 255, 255)']
+    ];
+
     var svg       = document.querySelector('svg');
     var container = svg.parentNode;
 
     var artSVG = new ArtSVG(container, svg, WIDTH, HEIGHT);
+
+    $('#color-stroke').spectrum({
+        preferredFormat      : 'rgb',
+        color                : 'rgba(0, 0, 0, 1.0)',
+        allowEmpty           : true,
+        showInput            : true,
+        showAlpha            : true,
+        showPalette          : true,
+        pallete              : PALLETE,
+        showSelectionPalette : true,
+        hide                 : function(color) {
+            artSVG.setStroke(color.toRgbString());
+        }
+    });
 
 });
